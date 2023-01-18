30917a7918724318f0ecdca4110f53b041ae3529
Eveline Huiser <evelinehuiser@gmail.com>
2023-01-18 15:35:06 +0100

    all done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  resources :restaurants, except: [:destroy, :edit, :update] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+choux = { name: 'Choux', address: 'De Ruijterkade 128, 1011 AC Amsterdam', category: 'french'}
+bar_centraal = { name: 'Bar Centraal', address: 'Ten Katestraat 16, 1053 CE Amsterdam', category: 'french' }
+pazzi = { name: 'Pazzi', address: 'De Clercqstraat 93III, 1053 AG Amsterdam', category: 'italian' }
+wolf = { name: 'Cafe Wolf', address: 'Wolvenstraat 22, 1016 EP Amsterdam', category: 'french' }
+taste_of_culture = { name: 'Taste of Culture', address: 'Korte Leidsedwarsstraat 139-141, 1017 PZ Amsterdam',
+                     category: 'chinese' }
+
+restaurants = [choux, bar_centraal, pazzi, wolf, taste_of_culture]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished'
